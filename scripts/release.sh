d63b1e77de5c1ddfb20ebe4d3da646689295163e
Kevin Schoonover <me@kschoon.me>
2023-03-24 18:09:06 -0700

    fix release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,6 @@
 VERSION=0.0.9
 
-pushd ./infrastructure 
-
 earthly +build && \
   cp ./bin/bloominlabs-otel-collector ./bin/bloominlabs-otel-collector-linux-amd64 && \
   sha256sum ./bin/bloominlabs-otel-collector-linux-amd64 > ./bin/SHA256SUMS && \
   gh release -R bloominlabs/bloominlabs-otel-collector create --title v$VERSION v$VERSION ./bin/*
-
-popd 
